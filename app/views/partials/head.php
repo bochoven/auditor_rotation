6344e5ff1c9203927ec3355460d3d90c5f03520e
Arjen van Bochoven <arjen@bochoven.net>
2013-04-16 21:28:32 +0200

    Fixed title

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Auditor Rotation</title>
+    <title><?=$GLOBALS['sitename']?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
